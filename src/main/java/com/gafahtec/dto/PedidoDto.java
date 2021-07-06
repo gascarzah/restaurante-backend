2eca8c9f674c4a7e35790e9664482202faac5d1d
Giovanni Ascarza <gascarzah@gmail.com>
2021-07-06 12:00:09 -0500

    modificado en carabayllo

---
+++ src/main/java/com/gafahtec/dto/PedidoDto.java
@@ -0,0 +1,16 @@
+package com.gafahtec.dto;
+
+import java.util.List;
+
+import com.gafahtec.model.Mesa;
+import com.gafahtec.model.Pedido;
+import com.gafahtec.model.PedidoDetalle;
+
+import lombok.Data;
+@Data
+public class PedidoDto {
+
+	private Pedido pedido;
+	private List<PedidoDetalle> pedidoDetalles;
+	private List<Mesa> mesas;
+}
